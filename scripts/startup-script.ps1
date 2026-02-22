80ed01021e52c20a656b153053f9a33683f6b789
anthonymora-tech <anthony.mora.tech@gmail.com>
2026-02-22 02:12:20 -0800

    Add startup logging script for domain computers

--- scripts/startup-script.ps1
+++ scripts/startup-script.ps1
@@ -1,1 +1,7 @@
+# Startup Script: Log machine startup to a shared folder
 
+$LogPath = "\\dc1\SYSVOL\lab.local\scripts\startup-log.txt"
+$ComputerName = $env:COMPUTERNAME
+$Date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+Add-Content -Path $LogPath -Value "$ComputerName started at $Date"
